b369b99ea40069da2c39cbdebd6a604930a323cb
franandroic <fran.androic@fer.hr>
2023-10-21 00:53:49 +0200

    Osvjezena python datoteka, program ima osnovnu funkcionalnost

--- lab1/py-ferlab.py
+++ lab1/py-ferlab.py
@@ -1,10 +1,9 @@
 import requests
 
-
+'''
 #App specifics
 client_id = "0YQDH9MopcVypOK4ZE4d1Q"
 client_secret = "y6LazDUW72esF8eNLZCPp8MQXJeDpw"
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 
 #Authentication
 auth_url = "https://www.reddit.com/api/v1/access_token"
@@ -13,16 +12,21 @@ auth_data = {
     "username": client_id,
     "password": client_secret
 }
-auth_headers = {"User-Agent": user_agent}
 
 auth_response = requests.post(auth_url, data=auth_data, auth=(client_id, client_secret), headers=auth_headers)
 auth_json = auth_response.json()
 access_token = auth_json["access_token"]
+'''
 
-#Getting the date from Reddit
-reddit_url = "https://www.reddit.com/r/dataengineering/top.json/?t=all&limit=10"
-data = requests.get(reddit_url).json()
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+auth_headers = { "User-Agent": user_agent }
 
-for post in data["data"]["children"]:
-    print(post["data"]["title"])
-    print()
+#Getting the data from Reddit
+reddit_url = "https://www.reddit.com/r/dataengineering/top.json?t=all&limit=10"
+response = requests.get(reddit_url, headers=auth_headers)
+
+if response.ok:
+    data = response.json()
+    print(data)
+else:
+    print(response.status_code)
